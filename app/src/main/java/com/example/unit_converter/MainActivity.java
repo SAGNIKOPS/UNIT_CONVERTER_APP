4dc55172266d09c6e667975aa32ef762801f64e3
SAGNIKOPS <sagnikb52@gmail.com>
2023-11-08 18:05:21 +0530

    Final Unit Converter App

---
+++ app/src/main/java/com/example/unit_converter/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.unit_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = editText.getText().toString();
+                double doubleValue = Double.parseDouble(value);
+                textView.setText(convertIntoPounds(doubleValue)+" pounds");
+            }
+        });
+
+    }
+    public double convertIntoPounds(double Value)
+    {
+        return Value*2.20462;
+    }
+}
